f82808c547ac3152d37f75705489de82bf565f80
Ashton <92445005+ashton8504@users.noreply.github.com>
2022-02-16 17:22:03 -0500

    updating JS file

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ let pokemonList = [
 ];
 
 for(let i=0; i < pokemonList.length; i++){
-  if (pokemonList[i].height > 10){
-    document.write(pokemonList[i].name + (height: + pokemonList[i].height) + "Wow, that's a big Pokemon!")
+  if (pokemonList[i].height > 1){
+    document.write(pokemonList[i].name +  (' height: ' + pokemonList[i].height)  +  ( " Wow, that's a big Pokemon! " ))
   }
 }
