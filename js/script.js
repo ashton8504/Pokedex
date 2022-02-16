f4991927c4c0cbaee2d1eb7dc693b58ab0a642c0
Ashton <92445005+ashton8504@users.noreply.github.com>
2022-02-16 11:05:19 -0500

    adding pokemon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
+//Pokemon List//
 
-alert("Hello World");
+let pokemonList = [{
+  name: "Venusaur",
+  height: 2,
+  weight: 100,
+  types: ['grass','poison'],
+  abilities:['chlorophyll','overgrow']
+},
 
-let favoriteFood = "Pizza"
-document.write(favoriteFood)
+{
+  name: "Pikachu",
+  height: .4,
+  weight: 6,
+  types: ['field', 'fairy'],
+  abilities:['static','lightingrod']
+},
+
+{
+  name: "Blastoise",
+  height: 1.6,
+  weight: 85.5,
+  types:['monster', 'water'],
+  abilities:['rain-dish', 'torrent']
+},
+
+{
+  name: "Charizard",
+  height: 1.7,
+  weight: 90.5,
+  types:['monster', 'dragon'],
+  abilities:['blaze', 'solar-power']
+}
+];
