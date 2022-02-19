f020f72a4a56604baed1bc5735cfed7ae14ab6f9
Ashton <92445005+ashton8504@users.noreply.github.com>
2022-02-19 10:30:55 -0500

    adding updates

--- js/script.js
+++ js/script.js
@@ -1,41 +1,104 @@
-//Pokemon List//
+//IIFE//
+let pokemonRepository = (function(){
+  //Pokemon List Variables//
+  let pokemonList = [
+    {
+    name: "Venusaur",
+    height: 2,
+    weight: 100,
+    types: ['grass','poison'],
+    abilities:['chlorophyll','overgrow']
+  },
 
-let pokemonList = [
   {
-  name: "Venusaur",
-  height: 2,
-  weight: 100,
-  types: ['grass','poison'],
-  abilities:['chlorophyll','overgrow']
-},
+    name: "Pikachu",
+    height: .4,
+    weight: 6,
+    types: ['field', 'fairy'],
+    abilities:['static','lightingrod']
+  },
 
-{
-  name: "Pikachu",
-  height: .4,
-  weight: 6,
-  types: ['field', 'fairy'],
-  abilities:['static','lightingrod']
-},
+  {
+    name: "Blastoise",
+    height: 1.6,
+    weight: 85.5,
+    types:['monster', 'water'],
+    abilities:['rain-dish', 'torrent']
+  },
 
-{
-  name: "Blastoise",
-  height: 1.6,
-  weight: 85.5,
-  types:['monster', 'water'],
-  abilities:['rain-dish', 'torrent']
-},
-
-{
-  name: "Charizard",
-  height: 1.7,
-  weight: 90.5,
-  types:['monster', 'dragon'],
-  abilities:['blaze', 'solar-power']
-}
+  {
+    name: "Charizard",
+    height: 1.7,
+    weight: 90.5,
+    types:['monster', 'dragon'],
+    abilities:['blaze', 'solar-power']
+  }
 ];
 
-for(let i=0; i < pokemonList.length; i++){
-  if (pokemonList[i].height > 1){
-    document.write(pokemonList[i].name +  (' height: ' + pokemonList[i].height)  +  ( " Wow, that's a big Pokemon! " ))
-  }
-}
+  // function getAll {
+  //   return pokemonList;
+  // }
+  // function add (pokemon){
+  //   pokemonList.push(pokemon)
+  // }
+  return {
+    getAll: function(){
+      return pokemonList;
+    };
+    add: function(pokemon){
+      pokemonList.push(pokemon);
+    }
+  };
+
+})();
+
+console.log(pokemonRepository.getAll())
+
+// //Pokemon List Variables//
+// let pokemonList = [
+//   {
+//   name: "Venusaur",
+//   height: 2,
+//   weight: 100,
+//   types: ['grass','poison'],
+//   abilities:['chlorophyll','overgrow']
+// },
+//
+// {
+//   name: "Pikachu",
+//   height: .4,
+//   weight: 6,
+//   types: ['field', 'fairy'],
+//   abilities:['static','lightingrod']
+// },
+//
+// {
+//   name: "Blastoise",
+//   height: 1.6,
+//   weight: 85.5,
+//   types:['monster', 'water'],
+//   abilities:['rain-dish', 'torrent']
+// },
+//
+// {
+//   name: "Charizard",
+//   height: 1.7,
+//   weight: 90.5,
+//   types:['monster', 'dragon'],
+//   abilities:['blaze', 'solar-power']
+// }
+// ]};
+
+//for loop example//
+// for(let i=0; i < pokemonList.length; i++){
+//   if (pokemonList[i].height > 1){
+//     document.write(pokemonList[i].name +  (' height: ' + pokemonList[i].height)  +  ( " Wow, that's a big Pokemon! " ))
+//   }
+// }
+
+// //forEach()chlorophyll
+//   pokemonList.forEach(function(user){
+//     console.log(pokemon.name + pokemon.height);
+//   })
+//
+//   pokemonList.forEach()
