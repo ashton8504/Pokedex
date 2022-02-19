bcf0c89034e3834b15c661a6348fd8b28c8114bd
Ashton <92445005+ashton8504@users.noreply.github.com>
2022-02-19 11:58:16 -0500

    updating for final review

--- js/script.js
+++ js/script.js
@@ -45,6 +45,13 @@ return {
     };
 })();
 
+// for(let i=0; i < pokemonRepository.length; i++){
+//   if (pokemonList[i].height > 1){
+//     document.write(pokemonList[i].name +  (' height: ' + pokemonList[i].height)  +  ( " Wow, that's a big Pokemon! " ))
+  pokemonRepository.getAll().forEach(function(pokemon){
+document.write( " " + pokemon.name + " " + pokemon.type + " " + pokemon.height)
+});
+
 
 
 console.log(pokemonRepository.getAll());
