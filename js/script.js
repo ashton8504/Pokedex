58b1d32d701e94d8d980bf6aa96d40f5b05bca25
Ashton <92445005+ashton8504@users.noreply.github.com>
2022-02-21 11:44:02 -0500

    adding pokemon files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //IIFE//
 let pokemonRepository = (function(){
   //Pokemon List Variables//
-  let pokemonList = [
+  let repository = [
     {
     name: "Venusaur",
     height: 2,
@@ -35,74 +35,41 @@ let pokemonRepository = (function(){
   }
 ];
 
-return {
-    getAll:function() {
-      return pokemonList;
-    },
-    add:function(pokemon){
-      pokemonList.push(pokemon);
-    }
-    };
+function add(pokemon) {
+   if (
+     typeof pokemon === "object" &&
+     "name" in pokemon &&
+     "height" in pokemon &&
+     "types" in pokemon
+   ) {
+     repository.push(pokemon);
+   } else {
+     console.log("pokemon is not correct");
+   }
+ }
+ function getAll() {
+   return repository;
+ }
+ function addListItem(pokemon){
+   let pokemonList = document.querySelector(".pokemon-list");
+   let listpokemon = document.createElement("li");
+   let button = document.createElement("button");
+   button.innerText = pokemon.name;
+   button.classList.add("button-class");
+   listpokemon.appendChild(button);
+   pokemonList.appendChild(listpokemon);
+ }
+ return {
+   add: add,
+   getAll: getAll,
+   addListItem: addListItem
+ };
 })();
 
-// for(let i=0; i < pokemonRepository.length; i++){
-//   if (pokemonList[i].height > 1){
-//     document.write(pokemonList[i].name +  (' height: ' + pokemonList[i].height)  +  ( " Wow, that's a big Pokemon! " ))
-  pokemonRepository.getAll().forEach(function(pokemon){
-document.write( " " + pokemon.name + " " + pokemon.type + " " + pokemon.height)
+pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+ pokemonRepository.addListItem(pokemon);
 });
-
-
-
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Pikachu' });
-console.log(pokemonRepository.getAll());
-
-// //Pokemon List Variables//
-// let pokemonList = [
-//   {
-//   name: "Venusaur",
-//   height: 2,
-//   weight: 100,
-//   types: ['grass','poison'],
-//   abilities:['chlorophyll','overgrow']
-// },
-//
-// {
-//   name: "Pikachu",
-//   height: .4,
-//   weight: 6,
-//   types: ['field', 'fairy'],
-//   abilities:['static','lightingrod']
-// },
-//
-// {
-//   name: "Blastoise",
-//   height: 1.6,
-//   weight: 85.5,
-//   types:['monster', 'water'],
-//   abilities:['rain-dish', 'torrent']
-// },
-//
-// {
-//   name: "Charizard",
-//   height: 1.7,
-//   weight: 90.5,
-//   types:['monster', 'dragon'],
-//   abilities:['blaze', 'solar-power']
-// }
-// ]};
-
-//for loop example//
-// for(let i=0; i < pokemonList.length; i++){
-//   if (pokemonList[i].height > 1){
-//     document.write(pokemonList[i].name +  (' height: ' + pokemonList[i].height)  +  ( " Wow, that's a big Pokemon! " ))
-//   }
-// }
-
-// //forEach()chlorophyll
-//   pokemonList.forEach(function(user){
-//     console.log(pokemon.name + pokemon.height);
-//   })
-//
-//   pokemonList.forEach()
