68c1577b5b6a54380c3e821c821c05cdcf9b49ef
Ashton <92445005+ashton8504@users.noreply.github.com>
2022-03-01 17:07:24 -0500

    updating CSS file

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # A Pokedex for Gen 1 Pokemon
 
+<<<<<<< HEAD
 Pokedex is an app which lists over 150 different Pokemons(Gen 1).
 
 App has been developed using:
+=======
+Pokedex is an app which lists over 150 different Pokemons.
+
+App has been developed using:
+
+>>>>>>> 1b35a0b (updating CSS file)
 HTML CSS JavaScript jQuery Bootstrap
--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ let pokemonRepository = (function() { //This is anonymous since there is nothing
   function getAll() {
     return pokemonList;
   };
-
+ 
   function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
     let listpokemon = document.createElement("li");
@@ -60,7 +60,7 @@ let pokemonRepository = (function() { //This is anonymous since there is nothing
 
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
-      showModal(pokemon.name, pokemon.height, pokemon.imageUrl);
+      showModal(pokemon)
     });
   }
 
@@ -168,97 +168,3 @@ pokemonRepository.loadList().then(function () {
 
 
 
-// //Below is previous Code I am saving in case I messed up
-// let pokemonRepository = (function () {
-//   let pokemonList = [];
-//   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-//
-//   function add(pokemon) {
-//     if (
-//       typeof pokemon === "object" &&
-//       "name" in pokemon
-//     ) {
-//       pokemonList.push(pokemon);
-//     } else {
-//       console.log("Pokemon is not correct");
-//     }
-//   }
-//   function getAll() {
-//     return pokemonList;
-//   };
-//
-//   function addListItem(pokemon) {
-//     let pokemonList = document.querySelector(".pokemon-list");
-//     let listpokemon = document.createElement("li");
-//     let button = document.createElement("button");
-//     button.innerText = pokemon.name;
-//     button.classList.add("button-class");
-//     listpokemon.appendChild(button);
-//     pokemonList.appendChild(listpokemon);
-//     button.addEventListener("click", function(event) {
-//       showDetails(pokemon);
-//     });
-//   }
-//
-//   function loadList() {
-//     return fetch(apiUrl).then(function (response) {
-//       return response.json();
-//     }).then(function (json) {
-//       json.results.forEach(function (item) {
-//         let pokemon = {
-//           name: item.name,
-//           detailsUrl: item.url
-//         };
-//         add(pokemon);
-//         console.log(pokemon);
-//       });
-//     }).catch(function (e) {
-//       console.error(e);
-//     })
-//   }
-//
-//   function loadDetails(item) {
-//     let url = item.detailsUrl;
-//     return fetch(url).then(function (response) {
-//       return response.json();
-//     }).then(function (details) {
-//       // Now we add the details to the item
-//       item.imageUrl = details.sprites.front_default;
-//       item.height = details.height;
-//       item.types = details.types;
-//     }).catch(function (e) {
-//       console.error(e);
-//     });
-//   }
-//
-//   function showDetails(item) {
-//     loadDetails(item).then(function () {
-//       showModal(item);
-//     });
-//   }
-//
-//   function showModal() {
-//     let modalContainer = document.querySelector('#modal-container');
-//     modalContainer.classList.add('is-visible');
-//   }
-//
-//   document.querySelector('#show-modal').addEventListener('click', () => {
-//     showModal();
-//   });
-//
-//   return {
-//     add: add,
-//     getAll: getAll,
-//     addListItem: addListItem,
-//     loadList: loadList,
-//     loadDetails: loadDetails,
-//     showDetails: showDetails
-//   };
-// })();
-//
-//
-// pokemonRepository.loadList().then(function () {
-//   pokemonRepository.getAll().forEach(function (pokemon) {
-//     pokemonRepository.addListItem(pokemon);
-//   });
-// });
