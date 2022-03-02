b2ebc5fda6fb9b4491e736ad229d9930cad1692a
Ashton <92445005+ashton8504@users.noreply.github.com>
2022-03-02 10:10:24 -0500

    updated file and still cannot get pokemon images to load, saving progress

--- js/script.js
+++ js/script.js
@@ -5,22 +5,25 @@ let pokemonRepository = (function() { //This is anonymous since there is nothing
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; //API source where we get the pokemon info
 
+
   function add(pokemon) { //this function will add the value pokemon to the pokemonList with the push function
    pokemonList.push(pokemon);
   }
-
 
   //This returns all data from Pokemon List//
   function getAll() {
     return pokemonList;
   };
 
+//Creates a buttons with Pokemon names
   function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
     let listpokemon = document.createElement("li");
     let button = document.createElement("button");
+
     button.innerText = pokemon.name;
     button.classList.add("button-class");
+
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
     button.addEventListener("click", function(event) {
@@ -46,15 +49,15 @@ let pokemonRepository = (function() { //This is anonymous since there is nothing
     })
   }
 
-  function loadDetails(item) {
-    let url = item.detailsUrl;
+  function loadDetails(pokemon) {
+    let url = pokemon.detailsUrl;
     return fetch(url).then(function (response) {
       return response.json();
     }).then(function (details) {
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.types = details.types;
-      item.weight = details.weight;
+      pokemon.imageUrl = details.sprites.front_default;
+      pokemon.height = details.height;
+      pokemon.types = details.types;
+      pokemon.weight = details.weight;
     }).catch(function (e) {
       console.error(e);
     });
@@ -67,26 +70,31 @@ let pokemonRepository = (function() { //This is anonymous since there is nothing
   }
 
 //Bootstrap Modal?//
-  function showModal(item) {
-    let modalBody = $(".modal-body"); //modalBody
-    let modalTitle = $(".modal-title");//modalTitle
-    let modalHeader = $(".modal-header");//modalHeader
+  function showModal(pokemon) {
+    let modalBody = $(".modal-body"); //modalBody calling bootstrap div class in html
+    let modalTitle = $(".modal-title");//modalTitle calling bootstrap div class in html
+    let modalHeader = $(".modal-header");//modalHeader calling bootstrap div class in html
 
     modalTitle.empty();//clears the modalTitle after you click out
     modalBody.empty();//clears the modalBody after you click out
 
-    let nameElement = $("<h1>" + item.name + "</h1>"); //pokemon name element
+    let nameElement = $("<h1>" + pokemon.name + "</h1>"); //pokemon name element
+
     let imageElementFront = $('<img class="modal-img" style="width:50%">'); //pokemon image element
+    imageElementFront.attr("src", pokemon.imageUrlFront)
+
     let imageElementBack = $('<img class="modal-img" style="width:50%">');//pokemon image element
-    let heightElement =$("<p>" + "Height : " + item.height + "</p>");//pokemon height element
-    let weightElement = $("<p>" + "Weight : " + item.weight + "</p>");//pokemon weight element
-    let typesElement = $("<p>" + "Types : " + item.types + "</p>"); //pokemon type element
-    let abilitiesElement = $("<p>" + "Abilities : " + item.abilities + "</p>");//pokemon abilities element
+    imageElementBack.attr("src", pokemon.imageUrlBack)
+
+    let heightElement =$("<p>" + "Height : " + pokemon.height + "</p>");//pokemon height element
+    let weightElement = $("<p>" + "Weight : " + pokemon.weight + "</p>");//pokemon weight element
+    let typesElement = $("<p>" + "Types : " + pokemon.types + "</p>"); //pokemon type element
+    let abilitiesElement = $("<p>" + "Abilities : " + pokemon.abilities + "</p>");//pokemon abilities element
 
 
 
 
-//Appending the let functions above//
+//Appending the let functions above, this should display attributes of pokemon & image//
     modalTitle.append(nameElement);
     modalBody.append(imageElementFront);
     modalBody.append(imageElementBack);
@@ -94,12 +102,6 @@ let pokemonRepository = (function() { //This is anonymous since there is nothing
     modalBody.append(weightElement);
     modalBody.append(typesElement);
     modalBody.append(abilitiesElement);
-    // modal.append(imageElement);
-    // modal.appendChild(closeButtonElement);
-    // modal.appendChild(titleElement);
-    // modal.appendChild(contentElement);;
-    // modalContainer.appendChild(modal);
-    // modalContainer.classList.add('is-visible');
 }
   return {
     add: add,
