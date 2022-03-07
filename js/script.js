407d9abb1252709d87cfc5b2b0ddf9eb6d7e711e
Ashton <92445005+ashton8504@users.noreply.github.com>
2022-03-07 12:20:47 -0500

    adding CSS styles

--- js/script.js
+++ js/script.js
@@ -87,8 +87,8 @@ let pokemonRepository = (function() { //This is anonymous since there is nothing
 
     let heightElement =$("<p>" + "Height : " + pokemon.height + "</p>");//pokemon height element
     let weightElement = $("<p>" + "Weight : " + pokemon.weight + "</p>");//pokemon weight element
-    // let typesElement = $("<p>" + "Types : " + pokemon.types + "</p>"); //pokemon type element
-    // let abilitiesElement = $("<p>" + "Abilities : " + pokemon.abilities + "</p>");//pokemon abilities element
+    let typesElement = $("<p>" + "Types : " + pokemon.types + "</p>"); //pokemon type element
+    let abilitiesElement = $("<p>" + "Abilities : " + pokemon.abilities + "</p>");//pokemon abilities element
     let myModalInformation = $("#pokemonModal").modal('show');
 
 
